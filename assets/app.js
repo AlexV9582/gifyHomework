998892e03e47016cd763fa3580463949ddaf473d
Alexander Vincent <AlexV9582@yahoo.com>
2017-07-11 00:59:37 -0400

    I give up

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,40 @@
 var topics;
 var topicsArray = [];
-var gifTopic;
-var queryUrl = "https://api.giphy.com/v1/gifs/search" + "?q=" + gifTopic + "&?limit=10" + "&?rating=g" + "&apikey=dc6zaTOxFJmzC"
-
+var queryUrl = "https://api.giphy.com/v1/gifs/search?q=undefined&limit=10&rating=g&apikey=dc6zaTOxFJmzC"
+// Console.log query results to verify object layout
 $.ajax({
 		url: queryUrl,
 		method: "GET"
 	}).done(function(response) {
 		console.log(response)
 })		
-
+//On click function to create new buttons based on user input
 $("#getGifs").on("click", function(){
-		topics = $("#description").val().trim();
-		topicsArray.push(topics);
-		var gifButton = $("<button>").text(topics).attr("id", topics)
-		$("#holder").prepend(gifButton)
+	topics = $("#description").val().trim();
+	topicsArray.push(topics);
+	var gifButton = $("<button>").text(topics).attr("value", topics);
+	$("#holder").prepend(gifButton);
 })
-
-$("#" + topics).on("click", function() {
-	gifTopic = $(this).id()
-	var queryUrl = "https://api.giphy.com/v1/gifs/search" + "?q=" + gifTopic + "&?limit=10" + "&?rating=g" + "&apikey=dc6zaTOxFJmzC"
-	$.ajax({
-		url: queryUrl,
-		method: "GET"
-	}).done(function(response) {
-		console.log(response)
-		for (var i = 0; i < response.data.length; i++) {
-			var gifDiv = $("<div>")
-			var p = $("<p>").text("Rating: " + response.data[i].rating)
-			var gifImage = $("<img>")
-			gifImage.attr("src", response.data[i].images.fixed_height.url)
-			$("#holder").prepend(gifImage)
-			$("#holder").prepend(p)
-		}
-	})
+//On click function to generate and prepend gifs based on value of created button
+$("button").on("click", function() {
+	if (this.value != "default") {
+		var gifTopic = $(this).attr("value");
+		console.log(gifTopic)
+		var queryUrl = "https://api.giphy.com/v1/gifs/search" + "?q=" + gifTopic + "&limit=10" + "&rating=g" + "&apikey=dc6zaTOxFJmzC"
+		$.ajax({
+			url: queryUrl,
+			method: "GET"
+		}).done(function(response) {
+			console.log(response)
+			for (var i = 0; i < response.data.length; i++) {
+				var gifDiv = $("<div>")
+				var p = $("<p>").text("Rating: " + response.data[i].rating)
+				var gifImage = $("<img>")
+				gifImage.attr("src", response.data[i].images.fixed_height.url)
+				gifDiv.prepend(gifImage)
+				gifDiv.prepend(p)
+				$("holder").prepend(gifDiv)
+			}
+		})
+	}	
 })
