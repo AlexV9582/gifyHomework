f689c824c605e5b84e701823b7c072d3425bb4fb
Alexander Vincent <AlexV9582@yahoo.com>
2017-07-10 23:14:37 -0400

    Why doesn't this work...

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,37 @@
 var topics;
 var topicsArray = [];
-var queryUrl = "https://api.giphy.com/v1/gifs/search" + "?q=" + topics + "&?limit=10" + "&?rating=g" + "&apikey=dc6zaTOxFJmzC"
+var gifTopic;
+var queryUrl = "https://api.giphy.com/v1/gifs/search" + "?q=" + gifTopic + "&?limit=10" + "&?rating=g" + "&apikey=dc6zaTOxFJmzC"
 
 $.ajax({
-	url: queryUrl,
-	method: "GET"
-}).done(function(response) {
-	console.log(response)
-	$("#getGifs").on("click", function(){
+		url: queryUrl,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response)
+})		
+
+$("#getGifs").on("click", function(){
 		topics = $("#description").val().trim();
 		topicsArray.push(topics);
-			var gifButton = $("<button>").text(topics)
-			$("#holder").prepend(gifButton)
-	})
+		var gifButton = $("<button>").text(topics).attr("id", topics)
+		$("#holder").prepend(gifButton)
+})
 
+$("#" + topics).on("click", function() {
+	gifTopic = $(this).id()
+	var queryUrl = "https://api.giphy.com/v1/gifs/search" + "?q=" + gifTopic + "&?limit=10" + "&?rating=g" + "&apikey=dc6zaTOxFJmzC"
+	$.ajax({
+		url: queryUrl,
+		method: "GET"
+	}).done(function(response) {
+		console.log(response)
+		for (var i = 0; i < response.data.length; i++) {
+			var gifDiv = $("<div>")
+			var p = $("<p>").text("Rating: " + response.data[i].rating)
+			var gifImage = $("<img>")
+			gifImage.attr("src", response.data[i].images.fixed_height.url)
+			$("#holder").prepend(gifImage)
+			$("#holder").prepend(p)
+		}
+	})
 })
