0976f5ab9fa45fa4802f19d169e3fefa848f018f
Alexander Vincent <AlexV9582@yahoo.com>
2017-07-10 22:36:25 -0400

    creates buttons

--- assets/app.js
+++ assets/app.js
@@ -7,14 +7,11 @@ $.ajax({
 	method: "GET"
 }).done(function(response) {
 	console.log(response)
-	$("button").on("click", function(){
+	$("#getGifs").on("click", function(){
 		topics = $("#description").val().trim();
 		topicsArray.push(topics);
-		for (var i = 0; i < topicsArray.length; i++) {
-			var gifButton = $("<button>").attr("text", topicsArray[i])
+			var gifButton = $("<button>").text(topics)
 			$("#holder").prepend(gifButton)
-
-		}
-
 	})
+
 })
